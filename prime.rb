c267c9bbeaee52a0dbea4b0b1cb5376ca4eb20da
HFelixH <felix-hoang@outlook.com>
2022-09-20 19:15:13 +0930

    commit tree 12e97d3bd8f1729c24864cc3093e637052c61466

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+    if num < 0 or num == 0 or num == 1
+        return false
+    else
+        (2..num-1).to_a.all? do |single_factor|
+            num % single_factor != 0
+        end
+    end
+end
